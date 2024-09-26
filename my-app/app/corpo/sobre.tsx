5603e9d8f89a430e8d332114ced81c8d43189124
Gabriel Mendes <131783183+Mendecos@users.noreply.github.com>
2024-09-26 02:24:01 +0000

    cabeçalho

--- my-app/app/corpo/sobre.tsx
+++ my-app/app/corpo/sobre.tsx
@@ -4,8 +4,10 @@ import { View, Text, Image, Button, StyleSheet } from 'react-native';
 export default function sobre() {
   return (
     <View>
-        <Text>SOBRE A INDIE</Text>
-      
+      <View>
+        <Text>Encontre o que procura na Indie!</Text>
+        
+      </View>
     </View>
 
   );
--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,22 +1,46 @@
 import React from 'react';
-import { View, Text, Image, Button, StyleSheet } from 'react-native';
+import { View, Text, Image, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import Sobre from './corpo/sobre';
 
 const Header = () => {
   return (
-    <View style={styles.header}>
-      <Image
-        source={{ uri: 'logo.png' }}
-        style={styles.logo}
-      />
-      <View>
-        
+    <View>
+      <View style={styles.header}>
+        <View style={styles.logotipo}>
+          <Image
+            source={require('./img/logo.png')} // Caminho para a imagem local
+            style={styles.logo}
+          />
+          <Image
+            source={require('./img/logoescrito.png')} // Caminho para a imagem local
+            style={styles.logoEscrito}
+          />
+        </View>
+        <View>
+        </View>
+        <View style={styles.botoesContainer}>
+          <TouchableOpacity onPress={() => alert('Home clicado')}>
+            <Text style={styles.botaoTexto}>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert('Livros clicado')}>
+            <Text style={styles.botaoTexto}>Livros</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert('Sobre Nós clicado')}>
+            <Text style={styles.botaoTexto}>Sobre Nós</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert('Entrar clicado')} >
+            <Text style={styles.botaoEntrar}>Entrar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert('Entrar clicado')}>
+            <Text style={styles.botaoCad}>Cadastrar-se</Text>
+          </TouchableOpacity>
+
+
+        </View>
       </View>
-      <View style={styles.botoesContainer}>
-        <Button title="Entrar" onPress={() => alert('Entrar clicado')} />
-        <Button title="Login" onPress={() => alert('Login clicado')} />
-      </View>
-      
+      <Sobre />
+
+
     </View>
 
   );
@@ -28,16 +52,42 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 10,
-    backgroundColor: 'blue',
+    padding: 20,
+    backgroundColor: 'black',
+  },
+  logotipo: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  logoEscrito: {
+    width: 200,
+    height: 50,
   },
   logo: {
-    width: 40,
-    height: 40,
+    width: 90,
+    height: 60,
   },
   botoesContainer: {
     flexDirection: 'row',
+
   },
+  botaoTexto: {
+    marginHorizontal: 15, // Espaçamento entre os textos (botões)
+    color: 'white'
+  },
+  botaoEntrar: {
+    marginHorizontal: 15,
+    color: 'orange',
+  },
+  botaoCad: {
+    backgroundColor: 'purple',
+    color: 'white',
+    padding: 8,
+    borderRadius: 5,
+    marginHorizontal: 15
+  }
+
+
 });
 
 export default Header;
