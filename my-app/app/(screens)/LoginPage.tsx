69af472b817be8ad23feb607e8d3c89ba4590294
Gabriel Mendes <131783183+Mendecos@users.noreply.github.com>
2024-10-16 01:38:59 +0000

    tela de login estilos não finalizados

--- my-app/app/(screens)/LoginPage.tsx
+++ my-app/app/(screens)/LoginPage.tsx
@@ -19,52 +19,76 @@ export default function LoginPage({ navigation }: { navigation: any }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="E-mail"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Senha"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        onSubmitEditing={handleLogin}
-      />
-      {wrongInput && (
-        <Text style={styles.alertText}>E-mail ou senha incorretos!</Text>
-      )}
-      <Button title="Acessar" onPress={handleLogin} />
+      <Text style={styles.textBox}>
+        Encontre descubra e desbrave novas leituras na IndieTora
+      </Text>
+      <View style={styles.loginBox}>
+        <Text style={styles.titleLogin}>Entre na sua conta</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          onSubmitEditing={handleLogin}
+        />
+        {wrongInput && (
+          <Text style={styles.alertText}>E-mail ou senha incorretos!</Text>
+        )}
+        <Button title="Acessar" onPress={handleLogin} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    padding: 16,
+    display: 'flex',
+    flexDirection:'row',
+    justifyContent:'center',
+    padding: 120,
+    backgroundColor: "rgba(255, 228, 48, 0.5)"
+
   },
-  title: {
-    fontSize: 24,
+  loginBox: {
+    marginLeft:40
+
+  },
+  textBox: {
+    marginRight:120,
+    backgroundColor: 'purple',
+    borderRadius: 12,
+    marginBottom: 60
+
+  },
+  titleLogin: {
+    fontSize: 34,
     marginBottom: 16,
     textAlign: "center",
+    fontWeight: "bold",
+    
   },
   input: {
     height: 40,
     borderColor: "#ccc",
     borderWidth: 1,
     marginBottom: 12,
-    paddingHorizontal: 8,
+    paddingHorizontal: 50,
+    textAlign: 'left',
+    backgroundColor: 'white'
   },
   alertText: {
     color: "red",
     marginHorizontal: "auto",
     marginBottom: 12,
   },
+  
 });
